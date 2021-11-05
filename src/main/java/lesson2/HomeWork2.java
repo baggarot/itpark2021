9cdde3faa26c579fbea79b18e97bb01101379283
baggarot <inead@mail.ru>
2021-11-05 21:39:56 +0700

    lesson 2: fixing problems

--- src/main/java/lesson2/HomeWork2.java
+++ src/main/java/lesson2/HomeWork2.java
@@ -8,19 +8,19 @@ public class HomeWork2 {
 
     /**
      * Формула расчета прямого гидравлического удара в стальной трубе
-     * @param l длина трубопровода от места удара до сечения, в котором поддерживается постоянное давление, м
-     * @param D диаметр трубы, м
-     * @param h толщина стенок трубы, м
-     * @return
+     * @param pipeLenght длина трубопровода от места удара до сечения, в котором поддерживается постоянное давление, м
+     * @param diameter диаметр трубы, м
+     * @param wallThickness толщина стенок трубы, м
+     * @return фазу гидравлического удара
      */
-    public static double waterHammer(double l, double D, double h){
-        double E = Math.pow(10, 9); // модуль объемной упругости воды
-        double speed = 2 * E / 1000;
-        double v = Math.sqrt(speed); // скорость распространения звука в жидкости
-        double Etr = 1 + 0.01 * D / h * 1;
-        double steel = Math.sqrt(Etr);
-        double Cu = v * (1 / steel); // скорость распространения ударной волны в трубопроводе
-        double T = 2 * l / Cu;
-        return T;
+    public static double waterHammer(double pipeLenght, double diameter, double wallThickness){
+        double bulkModulus = Math.pow(10, 9); // модуль объемной упругости воды
+        double speed = 2 * bulkModulus / 1000;
+        double soundSpeed = Math.sqrt(speed); // скорость распространения звука в жидкости
+        double elasticModulus = 1 + 0.01 * diameter / wallThickness * 1;
+        double steel = Math.sqrt(elasticModulus);
+        double shockWaveVelocity = soundSpeed * (1 / steel); // скорость распространения ударной волны в трубопроводе
+        double impactPhase = 2 * pipeLenght / shockWaveVelocity;
+        return impactPhase;
     }
 }
