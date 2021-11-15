06c5a702db85fc652043b60889a275a47ac4a803
baggarot <inead@mail.ru>
2021-11-15 23:40:57 +0700

    lesson 5

---
+++ src/main/java/lesson5/HomeWork.java
@@ -0,0 +1,33 @@
+package lesson5;
+
+import java.util.Scanner;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите значение числа n для получения значения последовательности Фибоначчи");
+        int n = in.nextInt();
+        System.out.println("Член n последовательности Фибоначчи равен " + fibonacciSequence(n));
+    }
+
+    public static int fibonacciSequence(int n){
+        int num = 0;
+        int fnum = 1;
+        int fibseq = 0;
+        if (n == 0){
+            return num;
+        } else{
+            if (n == 1){
+                return fnum;
+            } else{
+                for (int fseq = 2; fseq <= n; fseq++){
+                    fibseq = num + fnum;
+                    num = fnum;
+                    fnum = fibseq;
+                }
+                return fibseq;
+            }
+        }
+    }
+}
