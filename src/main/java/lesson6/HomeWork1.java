315ad4e6251d9aa4466e45024a12c912c4b04155
baggarot <inead@mail.ru>
2021-11-19 19:33:27 +0700

    lesson 6

--- src/main/java/lesson6/HomeWork1.java
+++ src/main/java/lesson6/HomeWork1.java
@@ -10,26 +10,26 @@ public class HomeWork1 {
         System.out.println("Введите значение n для определения длины массива");
         int n = in.nextInt();
         int[] arrayNumber = new int[n];
-        for (int i = 0; i < arrayNumber.length; i++){
+        for (int i = 0; i < arrayNumber.length; i++) {
             arrayNumber[i] = (int) (Math.random() * 1000);
         }
         System.out.println(Arrays.toString(arrayNumber));
 
         int max = arrayNumber[0];
-        for(int i = 0; i < arrayNumber.length; i++){
+        for (int i = 0; i < arrayNumber.length; i++) {
             max = Math.max(max, arrayNumber[i]);
         }
         System.out.println("Максимальное значение в массиве равно " + max);
 
         int min = arrayNumber[0];
-        for(int i = 0; i < arrayNumber.length; i++){
+        for (int i = 0; i < arrayNumber.length; i++) {
             min = Math.min(min, arrayNumber[i]);
         }
         System.out.println("Минимальное значение в массиве равно " + min);
 
         double mean = 0;
         int sum = 0;
-        for(int i = 0; i < arrayNumber.length; i++){
+        for (int i = 0; i < arrayNumber.length; i++) {
             sum += arrayNumber[i];
         }
         mean = sum / arrayNumber.length;
